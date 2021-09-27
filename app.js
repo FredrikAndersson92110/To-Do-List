25a9e05e8ad509a933299c0595ed44d2302e0ae6
fredrik Andersson <fandersson1121@gmail.com>
2021-09-27 23:39:21 +0200

    Add database, default list items, create, read and delete

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
 const express = require("express"); 
 const bodyParser = require("body-parser"); 
-const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose"); 
 
-mongoose.connect("mongodb://localhost:27017/todolistDB"); 
+const app = express(); 
 
 app.set("view engine", "ejs"); 
-
 app.use(express.urlencoded({extended: true})); 
 app.use(express.static("public")); 
+
+// DB 
+mongoose.connect("mongodb://localhost:27017/todolistDB"); 
+
+const itemSchema = new mongoose.Schema({
+    name: String
+});
+
+const Item = mongoose.model("Item", itemSchema); 
+
+const item1 = new Item({
+    name: "make coffe"
+});
+
+const item2 = new Item({
+    name: "Buy milk"
+}); 
+
+const item3 = new Item({
+    name: "Bake cookies"
+});
+
+const defaultItems = [item1, item2, item3]; 
+
+
 
 // GET 
 app.get("/", (req,res) => {
 
-    const day = date.getDay(); 
-
-    res.render("list", {listTitle: day, newListItems: items}); 
+    Item.find({}, (err, foundItems) => {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Succesfully added our 3 default items");
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("list", {listTitle: "Today", newListItems: foundItems}); 
+        }
+    }); 
 });
+
 
 app.get("/work", (req, res) => {
     res.render("list", {listTitle: "Work List", newListItems: workItems});
@@ -26,20 +61,29 @@ app.get("/about", (req,res) => {
     res.render("about");
 });
 
-// POST 
+// POST SAVE ITEM TO 
 app.post("/", (req, res) => {
-    console.log(req.body);
 
-    if (req.body.list === "Work") {
-        workItems.push(req.body.newItem);
-        res.redirect("/work");
-    } else {
-        items.push(req.body.newItem); 
-        res.redirect("/"); 
-    }
+    const itemName = req.body.newItem; 
+
+    const item = new Item({
+        name: itemName
+    });
+
+    Item.create(item);
+    res.redirect("/");
 });
 
+app.post("/delete", (req, res) => {
+    const itemId = req.body.checkbox; 
 
+    Item.deleteOners({_id: itemId}, (err) => {
+        if (!err) {
+            res.redirect("/");
+        }
+    }); 
+
+});
 
 
 
